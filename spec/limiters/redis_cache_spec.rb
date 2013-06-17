11921bfa9f23163fd2e903a533fa86be433dbbaf
Shane Moore <shane@converser.io>
2013-06-17 15:01:03 +0100

    Added a new test to check that the cache has an associated expiry time when a hit is detected

--- spec/limiters/redis_cache_spec.rb
+++ spec/limiters/redis_cache_spec.rb
@@ -1,18 +1,22 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 require 'rack/throttle'
 require 'redis'
+require 'date'
 
 describe "Redis Cache Interaction" do
   include Rack::Test::Methods
 
   def app
     @target_app ||= example_target_app
-    @app ||= Rack::Throttle::Interval.new(@target_app, :min => 0.1, :cache => Redis.new)
+    @app ||= Rack::Throttle::Hourly.new @target_app, :min => 0.1, :cache => Redis.new
     @cache = @app.options[:cache]
+    @app
   end
 
-  context "When the cache respondes to :expire (as redis does)"
   it "should set an expiry time" do
-
+    get "/foo"
+    cache_date_key = "127.0.0.1:#{Date.today}"
+    @cache.ttl(cache_date_key).should be_an_instance_of Fixnum
+    @cache.ttl(cache_date_key).should < @app.class.default_ttl
   end
 end
